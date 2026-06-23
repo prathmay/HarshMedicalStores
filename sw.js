591be74104a67ca207fec0d7ad37398b3af85912
Sheldor <74054038+prathmay@users.noreply.github.com>
2026-06-23 16:31:05 +0530

    Add service worker with install and fetch event handlers

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response('Offline');
+    })
+  );
+});
